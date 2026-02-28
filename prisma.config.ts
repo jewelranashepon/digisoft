69c6fe8bcdeb6033f3d80c2fbb96a095de08821c
Jewel Rana <jewelranashepon@gmail.com>
2026-02-28 19:48:43 +0600

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,30 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import { defineConfig } from "prisma/config";
+
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   migrations: {
+//     path: "prisma/migrations",
+//     seed: "tsx prisma/seed.ts",
+//   },
+//   datasource: {
+//     url: process.env.DATABASE_URL,
+//   },
+// });
+
 import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+
+// Load default .env automatically
+dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
-  },
   datasource: {
     url: process.env.DATABASE_URL,
+  },
+  migrations: {
+    path: "prisma/migrations",
+    seed: "npx tsx prisma/seed.ts",
   },
 });
